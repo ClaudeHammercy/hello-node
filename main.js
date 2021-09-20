82a0c9125047a968d0a1ab9ff2f2347fbb4417ee
Rashad Gant <83968751+rashadgant@users.noreply.github.com>
2021-09-20 12:21:21 -0400

    Number2

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-console.log("HH   HH        lll lll                                   lll      dd !!");
+console.log("HH   HH        lll lll         ww      ww                lll      dd !!");
 console.log("HH   HH   eee  lll lll  oooo   ww      ww   oooo  rr rr  lll      dd !!");
 console.log("HHHHHHH ee   e lll lll oo  oo  ww      ww  oo  oo rrr  r lll  dddddd !!");
 console.log("HH   HH eeeee  lll lll oo  oo   ww ww ww   oo  oo rr     lll dd   dd !!");
 console.log("HH   HH  eeeee lll lll  oooo     ww  ww     oooo  rr     lll  dddddd !!");
+
+console.log("HHHHHHHH                   oo        ");
+console.log("HH   HHH eeeeee ll lll     oo  wwwwww");
+console.log("HHHHHHHH     ee llllll oooooo      ww");
+console.log("HH       eeeeee ll lll oo  oo  wwwwww");
+console.log("HH       eeeeee ll  ll oooooo  wwwwww");
